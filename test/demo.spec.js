dcced21f33d3bab1c388fdf3b20af56863406ca4
hoshimado <xingyanhuan@yahoo.co.jp>
2022-06-12 10:53:35 +0900

    #1 向けに1つ目のテストを追加

---
+++ test/demo.spec.js
@@ -0,0 +1,16 @@
+
+var chai = require('chai');
+var expect = chai.expect;
+var Demo = require('../src/demo.js').Demo;
+
+
+describe('demo.js', function () {
+    describe('getMessage()', ()=>{
+        it('returns demo Message.', ()=>{
+            const demo = new Demo();
+
+            expect(demo.getMessage())
+            .to.be.equal('Hello GitHub Actions!');
+        })
+    });
+});
